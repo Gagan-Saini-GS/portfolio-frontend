c2e4b8eddfaa3c99b166df58876c5e3bc168a4d2
Gagan-Saini-GS <gagansaini10999@gmail.com>
2023-05-12 11:42:35 +0530

    First commit before deploying

---
+++ src/Components/App/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Navbar from "../Navbar/Navbar.js";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to={"/home"} />} />
+          <Route path="/home" element={<div>Home Page</div>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="nav-container">
+      <section className="logo-section">Gagan Saini</section>
+      <section className="menu-section">
+        <Link className="menu-item home" to={"/home"}>
+          Home
+        </Link>
+        {/* <Link className="menu-item" to={"/wish"}>
+          Wish!
+        </Link>
+        <Link className="menu-item" to={"/thoughts"}>
+          Thoughts
+        </Link>
+        <Link className="menu-item" to={"/about"}>
+          About
+        </Link> */}
+      </section>
+    </nav>
+  );
+}
