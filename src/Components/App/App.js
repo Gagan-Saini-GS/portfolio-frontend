3c4016b9ee4efdd4cd5e200f1fb53d33d7863c0a
Gagan-Saini-GS <gagansaini10999@gmail.com>
2023-05-12 12:34:04 +0530

    Home page added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,7 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import Home from "../Home/Home";
 
 function App() {
   return (
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
-import Gallery from "../Gallery/Gallery";
-import Timer from "../Timer/Timer";
+// import Gallery from "../Gallery/Gallery";
+// import Timer from "../Timer/Timer";
 
 export default function Home() {
   const year = new Date().getFullYear();
